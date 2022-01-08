c9732bdc2a656db663f4ffecba3de83e16b36961
Yannick Armand <yannick.armand@protonmail.com>
2022-01-08 23:05:46 +0100

    Typographie : couleurs plus lisible

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     fontFamily: {
       'source-serif-pro': ['Source Serif Pro', 'serif'],
     },
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.inherit'),
+            '--tw-prose-bullets': theme('colors.inherit'),
+            '--tw-prose-invert-body': theme('colors.inherit'),
+            '--tw-prose-invert-bullets': theme('colors.inherit'),
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
