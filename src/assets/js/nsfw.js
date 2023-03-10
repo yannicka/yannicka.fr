c0d3f3df1731ff2d7103a7e85d074e3ea472c96d
Yannick Armand <yannick.armand@protonmail.com>
2023-03-10 16:37:14 +0100

    Bouton NSFW avec des émojis

--- src/assets/js/nsfw.js
+++ src/assets/js/nsfw.js
@@ -3,7 +3,7 @@ const BLUR_CLASS = 'blur-3xl'
 const nsfwButton = document.querySelector('.js-nsfw-button')
 
 if (nsfwButton) {
-  nsfwButton.innerText = 'Afficher les images NSFW'
+  nsfwButton.innerText = '👀 Afficher les images NSFW'
 
   const nsfwImages = document.querySelectorAll('[data-nsfw="nsfw"]')
 
@@ -15,10 +15,10 @@ if (nsfwButton) {
     nsfwImages.forEach(el => {
       if (el.classList.contains(BLUR_CLASS)) {
         el.classList.remove(BLUR_CLASS)
-        nsfwButton.innerText = 'Masquer les images NSFW'
+        nsfwButton.innerText = '🙈 Masquer les images NSFW'
       } else {
         el.classList.add(BLUR_CLASS)
-        nsfwButton.innerText = 'Afficher les images NSFW'
+        nsfwButton.innerText = '👀 Afficher les images NSFW'
       }
     })
   })
